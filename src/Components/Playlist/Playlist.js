e9a7adfb76b8663734db02d5de834d41718dbef2
Chuck Gibson <90487306+Chuck-Gibson@users.noreply.github.com>
2022-11-15 08:59:51 -0600

    modified spacing

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -14,8 +14,8 @@ export class Playlist extends React.Component{
         return(
             <div className="Playlist">
                 <input defaultValue={"New Playlist"} onChange= {this.handleNameChange}/>
-                <TrackList tracks={this.props.playListTracks} onRemove = {this.props.onRemove} isRemoval = {true} />
-                <button className="Playlist-save" onClick= {this.props.onSave}>SAVE TO SPOTIFY</button>
+                <TrackList tracks= {this.props.playListTracks} onRemove= {this.props.onRemove} isRemoval = {true} />
+                <button className= "Playlist-save" onClick= {this.props.onSave}>SAVE TO SPOTIFY</button>
             </div>
         )
     }
